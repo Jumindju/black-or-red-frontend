a56862b2558b8da8b89d5f72beca7213255ba6b8
Justus Hueske <justus.hueske@studmail.w-hs.de>
2022-02-24 20:28:27 +0100

    :sparkles: Add scss

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import './header.scss';
+
 const Header = () => (
-  <header>
-    Header. TODO: Content
+  <header className="header">
+    <h2>
+      Header
+    </h2>
   </header>
 );
 
